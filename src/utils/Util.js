e9da3140cddb398302155fcd8fdad8c48d71cd67
fanhuaran <fanhuaran@zhihu.com>
2021-01-31 19:04:04 +0800

    feat: 准备上线

--- src/utils/Util.js
+++ src/utils/Util.js
@@ -2,18 +2,22 @@ import axios from "axios";
 
 class Util {
   static getQuestionnaires() {
-    return axios.get("/api/questionnaire_survey/all").then(result => {
-      const { data } = result;
-      if (data.code !== 20000) {
-        throw new Error("error");
-      }
-      return data.data;
-    });
+    return axios
+      .get("http://123.206.30.153:8080/api/questionnaire_survey/all")
+      .then(result => {
+        const { data } = result;
+        if (data.code !== 20000) {
+          throw new Error("error");
+        }
+        return data.data;
+      });
   }
 
   static getQuestions(id) {
     return axios
-      .get(`/api/questionnaire_survey/get?questionnaire_id=${id}`)
+      .get(
+        `http://123.206.30.153:8080/api/questionnaire_survey/get?questionnaire_id=${id}`
+      )
       .then(result => {
         const { data } = result;
         if (data.code !== 20000) {
@@ -24,13 +28,15 @@ class Util {
   }
 
   static commit(data) {
-    return axios.post("/api/questionnaire_survey/commit", data).then(result => {
-      const { data } = result;
-      if (data.code !== 20000) {
-        throw new Error(data);
-      }
-      return data.data;
-    });
+    return axios
+      .post("http://123.206.30.153:8080/api/questionnaire_survey/commit", data)
+      .then(result => {
+        const { data } = result;
+        if (data.code !== 20000) {
+          throw new Error(data);
+        }
+        return data.data;
+      });
   }
 }
 
